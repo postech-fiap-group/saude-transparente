2fd3e08bc361cb254886944873e7e934bf3412e4
armanoalves <armanobarrosalvesh@gmail.com>
2026-01-21 22:10:12 -0300

    First commit

---
+++ src/main/java/com/fiap/saude_transparente/SaudeTransparenteApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.saude_transparente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaudeTransparenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaudeTransparenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/saude_transparente/SaudeTransparenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.saude_transparente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaudeTransparenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
