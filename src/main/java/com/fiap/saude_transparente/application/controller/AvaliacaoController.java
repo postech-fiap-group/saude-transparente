f75bbf256d8702d6eb2bfbb924edbc1c3a598eec
leandro-sant <leandro19br@gmail.com>
2026-01-29 20:54:47 -0300

    refatorando removendo variável não utilizada

--- src/main/java/com/fiap/saude_transparente/application/controller/AvaliacaoController.java
+++ src/main/java/com/fiap/saude_transparente/application/controller/AvaliacaoController.java
@@ -25,64 +25,64 @@ import java.util.stream.Collectors;
 @Tag(name = "Avaliacoes", description = "Avaliacoes de pacientes")
 public class AvaliacaoController {
 
-	private final CriarAvaliacaoService criarAvaliacaoService;
-	private final AlterarAvaliacaoService alterarAvaliacaoService;
-	private final ObterTodasAvaliacoesService obterTodasAvaliacoesService;
-	private final ObterEstatisticaAvaliacaoPorMedicoId obterEstatisticaAvaliacaoPorMedicoId;
-	private final ObterAvaliacaoPorId obterAvaliacaoPorId;
-	private final AvaliacaoPresenter avaliacaoPresenter;
-	private final Validator validator;
+    private final CriarAvaliacaoService criarAvaliacaoService;
+    private final AlterarAvaliacaoService alterarAvaliacaoService;
+    private final ObterTodasAvaliacoesService obterTodasAvaliacoesService;
+    private final ObterEstatisticaAvaliacaoPorMedicoId obterEstatisticaAvaliacaoPorMedicoId;
+    private final ObterAvaliacaoPorId obterAvaliacaoPorId;
+    private final AvaliacaoPresenter avaliacaoPresenter;
+    private final Validator validator;
 
-	@PostMapping
-	public ResponseEntity<Void> avaliar(
-			@RequestBody @Valid CriarAvaliacaoDTO avaliacaoDTO){
+    @PostMapping
+    public ResponseEntity<Void> avaliar(
+            @RequestBody @Valid CriarAvaliacaoDTO avaliacaoDTO) {
 
-		var erros = this.validator.validateObject(avaliacaoDTO)
-				.getAllErrors()
-				.stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.toSet());
+        this.validator.validateObject(avaliacaoDTO)
+                .getAllErrors()
+                .stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.toSet());
 
-		this.criarAvaliacaoService.save(new CriarAvaliacaoCommand(avaliacaoDTO.consultaId()
-				, avaliacaoDTO.nota(), avaliacaoDTO.comentario()));
-		return ResponseEntity.status(200).build();
-	}
+        this.criarAvaliacaoService.save(new CriarAvaliacaoCommand(avaliacaoDTO.consultaId()
+                , avaliacaoDTO.nota(), avaliacaoDTO.comentario()));
+        return ResponseEntity.status(200).build();
+    }
 
-	@PutMapping("/{id}")
-	public ResponseEntity<Void> alterar(
-			@PathVariable("id") Long id,
-			@RequestBody CriarAvaliacaoDTO cardapioDTO) {
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> alterar(
+            @PathVariable("id") Long id,
+            @RequestBody CriarAvaliacaoDTO cardapioDTO) {
 
-		var erros = this.validator.validateObject(cardapioDTO)
-				.getAllErrors()
-				.stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.toSet());
+        this.validator.validateObject(cardapioDTO)
+                .getAllErrors()
+                .stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.toSet());
 
-		var cmd = new AlterarAvaliacaoCommand(id, cardapioDTO.consultaId(),
-				cardapioDTO.nota(), cardapioDTO.comentario());
+        var cmd = new AlterarAvaliacaoCommand(id, cardapioDTO.consultaId(),
+                cardapioDTO.nota(), cardapioDTO.comentario());
 
-		this.alterarAvaliacaoService.save(cmd);
+        this.alterarAvaliacaoService.save(cmd);
 
-		var status = HttpStatus.NO_CONTENT;
-		return ResponseEntity.status(status.value()).build();
-	}
+        var status = HttpStatus.NO_CONTENT;
+        return ResponseEntity.status(status.value()).build();
+    }
 
-	@GetMapping
-	public ResponseEntity<List<Avaliacao>> getAllAvaliacao(
-			@RequestParam(value = "page", defaultValue = "1") int page,
-			@RequestParam(value = "size", defaultValue = "10") int size) {
+    @GetMapping
+    public ResponseEntity<List<Avaliacao>> getAllAvaliacao(
+            @RequestParam(value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size) {
 
-		return ResponseEntity.ok(this.obterTodasAvaliacoesService.execute(page, size));
-	}
+        return ResponseEntity.ok(this.obterTodasAvaliacoesService.execute(page, size));
+    }
 
-	@GetMapping("/{id}")
-	public ResponseEntity<Avaliacao> getById(
-			@PathVariable("id") Long id) {
-		return ResponseEntity.ok(this.obterAvaliacaoPorId.execute(id));
-	}
+    @GetMapping("/{id}")
+    public ResponseEntity<Avaliacao> getById(
+            @PathVariable("id") Long id) {
+        return ResponseEntity.ok(this.obterAvaliacaoPorId.execute(id));
+    }
 
-	@GetMapping("/medico/{medicoId}")
-	public ResponseEntity<NotaResponse> getAvaliacaoPorMedicoId(
-			@PathVariable("medicoId") Long medicoId) {
+    @GetMapping("/medico/{medicoId}")
+    public ResponseEntity<NotaResponse> getAvaliacaoPorMedicoId(
+            @PathVariable("medicoId") Long medicoId) {
 
-		return ResponseEntity.ok(avaliacaoPresenter.presenter(this.obterEstatisticaAvaliacaoPorMedicoId.execute(medicoId)));
+        return ResponseEntity.ok(avaliacaoPresenter.presenter(this.obterEstatisticaAvaliacaoPorMedicoId.execute(medicoId)));
 
-	}
+    }
 }
